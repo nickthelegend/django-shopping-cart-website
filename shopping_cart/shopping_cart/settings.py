97b947c5c992f9829d8b778ac2b208310082d2e4
nickthelegend <testingtesla7@gmail.com>
2024-09-24 09:24:00 +0400

    Image 404 Fixed

--- shopping_cart/shopping_cart/settings.py
+++ shopping_cart/shopping_cart/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -69,7 +69,8 @@ TEMPLATES = [
 ]
 
 
-MEDIA_URL = './'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 WSGI_APPLICATION = 'shopping_cart.wsgi.application'
 
 
